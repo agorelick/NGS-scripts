cb44db81b4b0e3879a150db1ab9f7047d223d6e7
Alex Gorelick <agorelick@mgh.harvard.edu>
2023-10-06 14:34:34 -0400

    adding script to sweep dipLogRs for one sample to o2

---
+++ sweep_sample.R
@@ -0,0 +1,37 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# here I am attempting to use pseudoSNPs + phased SNP blocks
+# with mostly out of the box FACETS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library(QDNAseq)
+library(Rsamtools)
+library(data.table)
+library(Rcpp)
+library(Biobase)
+library(pctGCdata)
+library(here)
+library(ASCAT)
+
+source(here('func.R'))
+
+## which sample to run
+#args = commandArgs(trailingOnly=TRUE)
+#sample_index <- as.integer(args[1])
+sample_index <- 2
+
+## load data
+d <- fread('/home/alg2264/data/alex/lpASCN/C161/C161_q=10_Q=20_P=0_1000kbs_preprocessed.tsv')
+seg <- fread('/home/alg2264/data/alex/lpASCN/C161/C161_q=10_Q=20_P=0_1000kbs_segments.tsv')
+samples <- sort(unique(as.character(seg$sample)))
+this_sample <- samples[sample_index]
+output_file <- paste0('/home/alg2264/data/alex/lpASCN/C161/C161_q=10_Q=20_P=0_1000kbs_dipLogRsweep_',this_sample,'.tsv')
+
+## define dipLogR values to sweep through
+dipLogRs <- seq(-2,2,by=0.5)
+l <- sweep_sample(this_sample, sex='female', seg, d, dipLogRs, cores=6)
+
+## save result
+message('Saving the result to: ',output_file)
+saveRDS(l,file=output_file)
+
+
