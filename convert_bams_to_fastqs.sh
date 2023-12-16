06e52f20d7297bf01f0e1c04075c3cf38a1caecc
Alex Gorelick <agorelick@mgh.harvard.edu>
2023-12-16 12:17:43 -0500

    Update convert_bams_to_fastqs.sh
    updated to save fastqs to 00_fastq/*fastq.gz

--- convert_bams_to_fastqs.sh
+++ convert_bams_to_fastqs.sh
@@ -2,7 +2,7 @@
 #SBATCH -c 1
 #SBATCH -t 0-01:00
 #SBATCH -p short
-#SBATCH --mem 10000
+#SBATCH --mem 8000
 #SBATCH -o run_bam2fq_%A_%a.out
 #SBATCH --array=0-17
 #SBATCH --mail-user=alexander_gorelick@hms.harvard.edu  # Email to which notifications will be sent
@@ -11,7 +11,7 @@
 # This pipeline should be run from FASTQ files to ensure that all preprocessing is standardized. In case you only have access to BAM files and you aren't sure how they were generated, you can convert them back to FASTQs with this script.
 
 module load picard/2.27.5
-mkdir -p fastqs
+mkdir -p 00_fastq
 
 ## declare arrays
 declare -a samples=("C157B1" "C157B2" "C157Di1" "C157Di2" "C157LN4" "C157N1" "C157P10" "C157P1" "C157P2" "C157P3" "C157P4" "C157P6" "C157P9" "C157TD1" "C157TD2a" "C157TD2b" "C157TD3" "C157TD7")
@@ -21,6 +21,10 @@ sample=${samples[$i]}
 
 ## convert the bam file back to PE-read FASTQs
 BAM="original_bams/${sample}_aligned.bam"
-FQ1="fastqs/${sample}_1.fq"
-FQ2="fastqs/${sample}_2.fq"
+FQ1="00_fastq/${sample}_R1_001.fastq"
+FQ2="00_fastq/${sample}_R2_001.fastq"
 java -jar $PICARD/picard.jar SamToFastq I=$BAM FASTQ=$FQ1 SECOND_END_FASTQ=$FQ2
+
+## compress the fastq files
+gzip $FQ1
+gzip $FQ2
